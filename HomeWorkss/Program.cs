1415eac425068a0e7595ba0aec765b219ed00c95
Nika <nikafaxuridze@gmail.com>
2025-09-16 23:05:26 -0700

    add divisibility

--- HomeWorkss/Program.cs
+++ HomeWorkss/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Divisibility
+/*1. შეამოწმეთ იყოფა თუ არა რიცხვი 5ზე უნაშთოდ. რიცხვი უნდა მიიღოთ
+           კონსოლიდან.
+           Input : 10 Output : Yes
+           Input : 44 Output : NO*/
+        Console.Write("Enter a number to check if it is divisible by 5: ");
+        var number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 5 == 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+        
+
+        #endregion
     }
 }
